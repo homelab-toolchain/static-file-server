2cbeb10d782626fdedfb4683cd84bd686990fb2b
Nobody <s.ahmad2702@gmail.com>
2025-02-22 21:56:00 +0100

    chore: add missing tag

--- release.config.js
+++ release.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-    branches: ["main"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      ["@semantic-release/git", {
-        "assets": ["CHANGELOG.md"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]"
-      }],
-      "@semantic-release/github"
-    ]
-  };
+  branches: ["main"],
+  tagFormat: "v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    ["@semantic-release/git", {
+      "assets": ["CHANGELOG.md"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]"
+    }],
+    "@semantic-release/github"
+  ]
+};
